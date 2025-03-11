8cf6897589dd998215962076755ccf5b68d6d5c7
clairecleverlamb <clairechenszeying@gmail.com>
2025-03-11 14:47:01 -0700

    handle fruit adding

--- controllers/fruits.js
+++ controllers/fruits.js
@@ -5,12 +5,23 @@ const router = express.Router();
 const Fruit = require('../models/fruit.js');
 
 router.get('/new', (req, res) => {
-  res.render('fruits/new.ejs');
+  res.status(200).render('fruits/new.ejs');
 });
 
 router.post('/', async (req, res) => {
-  await Fruit.create(req.body);
-  res.redirect('/fruits');
+  try {
+    if(!req.body.name.trim()){
+        throw new Error("Invalid input: the name field cannot be empty");
+    }
+    await Fruit.create(req.body);
+    req.session.message = "Fruit successfully created!";
+    res.redirect('/fruits');
+  } catch (error) {
+    req.session.message = error.message;
+    // res.render('fruits/news.ejs', { errorMessage: error.message });
+    // res.render('error.ejs', {msg: error.message});
+    res.redirect('/fruits');
+  };
 });
 
 router.get('/', async (req, res) => {
